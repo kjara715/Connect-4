a46043b030f99bacf94cf8539cf99b8306cb0ff1
Katrina <katrinajara2@gmail.com>
2020-11-24 12:18:13 -0500

    fixes made to buttons and styling

--- connect4.js
+++ connect4.js
@@ -3,6 +3,33 @@ const height = 6;
 let currPlayer = 1; // active player: 1 or 2; starts ar 1
 const board = []; // array of rows, each row is array of cells  (board[y][x])
 
+const container = document.querySelector('.container')
+const startButton = document.querySelector('#startGame');
+startButton.addEventListener('click', e => {
+  e.preventDefault();
+  makeBoard(); //make js baord of arrays
+  makeHtmlBoard(container); //make HTML board (table)
+  startButton.remove();
+});
+
+
+const restartButtonDiv = document.createElement('div');
+const restartButton=document.createElement('button');
+restartButton.innerText = "Play Again"
+restartButton.addEventListener('click', function(){
+  restartButton.remove();
+  const allTRs=document.querySelectorAll('tr');
+  for(let tr of allTRs){
+    tr.remove()
+  }
+  const board=[];
+  makeBoard();
+  makeHtmlBoard(document.querySelector('.container'));
+})
+
+
+
+
 //makeBoard: create in-JS board structure:
 //board = array of rows, each row is array of cells  (board[y][x])
 
@@ -16,27 +43,48 @@ const makeBoard = () => {
   return board //final output of board
 }
 /** makeHtmlBoard: make HTML table and row of column tops. */
-const body = document.querySelector('body');
-const thePlayer = document.createElement('div');
-thePlayer.classList.add('whichPlayer')
-const playerNow = document.createElement('div'); //create div to display the current player to make next move
+ //create div to display the current player to make next move
 
 
-const makeHtmlBoard = () => {
-  const htmlBoard = document.querySelector('#board'); 
-  thePlayer.innerText = 'Your Turn:'
+
+const makeHtmlBoard = (container) => {
+
+  const htmlBoard = document.querySelector('#board');
+  
+  const thePlayer = document.createElement('div');
+  thePlayer.classList.add('whichPlayer');
+  thePlayer.innerText = 'Your Turn:';
+
+  const playerNow = document.createElement('div');
+  playerNow.classList.add('thisPlayer'); 
   playerNow.innerText = `Player ${currPlayer}`;
+
+  const key = document.createElement('div');
+  const space = document.createElement('br');
+  key.innerText = 'Player 1 is Yellow, Player 2 is Red';
+  key.classList.add('key');
+
+  container.append(thePlayer);
+  thePlayer.append(playerNow);
+ 
+  
+  container.append(space);
+  container.append(key);
+
+  
   const top = document.createElement("tr"); // top row to click on the play piece
+
   top.setAttribute("id", "column-top"); // creates the top row (<tr>) for out connect4 table
   top.addEventListener("click", handleClick); //EventListener added to excute the handleClick funtion if the top (row) is clicked --> so only works for top square...
+  htmlBoard.append(top); 
 
   for (let i = 0; i < width; i++) {
     const headCell = document.createElement("td"); //creates a data cell for every column (width)
     headCell.setAttribute("id", i);  // each headcell is given a unique id from 0 to the width (here 7 is width so will be 0 to 6)
-    headCell.innerText='Click to Drop Piece';
+    headCell.innerHTML= '&darr;';
     top.append(headCell); //each <td> is added to the top row
   }
-  htmlBoard.append(top); //this adds the top row with all of the headCells to the htmlBoard (existing html element)
+  //this adds the top row with all of the headCells to the htmlBoard (existing html element)
 
   // TODO: add comment for this code
   for (let i = 0; i < height; i++) {
@@ -77,21 +125,29 @@ const placeInTable = (y, x) => {
 
 //execute if game ends (either a win or a tie if board's filled)
 const endGame = msg => {
-    alert(msg)
+    alert(msg);
+    const thePlayer= document.querySelector('.whichPlayer');
+    thePlayer.remove();
+    const key=document.querySelector('.key');
+    key.remove();
+    const space = document.querySelector('br');
+    space.remove();
+    container.append(restartButtonDiv);
+    restartButtonDiv.append(restartButton);
 }
 
 //handleClick: handle click of column top to play piece 
 function handleClick(evt) {
   if(checkForWin()){ //prevents more clicks from being handled if a winner has already been declared
-    return alert('A player has already won. Refresh page to begin again');
+    return alert('A player has already won. Click "Play Again" for a new game');
   };
 
   if(checkForTie()){ //prevents more clicks from being handled if a tie has already happened
-    return alert('There are no more moves left. Refresh page to begin again');
+    return alert('There are no more moves left. Click "Play Again" for a new game');
   }
   // get x from ID of clicked cell
   const x = +evt.target.id;
-  console.log(x)
+  
   // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
   if (y === null) {
@@ -116,8 +172,8 @@ function handleClick(evt) {
     currPlayer = 1;
   }
 
-  playerNow.innerText = `Player ${currPlayer}`;
-  
+  const playerNow = document.querySelector('.thisPlayer');
+  playerNow.innerText = `Player ${currPlayer}`; //changes the innerText each turn to display the current player
 }
 
 function checkForTie(){
@@ -133,45 +189,30 @@ function checkForWin() {
     //  - cells: list of four (y, x) cells
     //  - returns true if all are legal coordinates & all match currPlayer
 
-    return cells.every(
+    return cells.every( 
       ([y, x]) =>
-        y >= 0 &&
+        y >= 0 && //confines of the y-coordinate is in between 0 and the height of board
         y < height &&
-        x >= 0 &&
+        x >= 0 && //confines of the x-coordinate is in between 0 and the height of board
         x < width &&
-        board[y][x] === currPlayer
+        board[y][x] === currPlayer  //does this spot in the board contain the currPlayer...if all 4 does, will return true
     );
   }
 
   // TODO: read and understand this code. Add comments to help you.
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]]; //
+      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
-      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) { //checks if any of the four possibilities are true, meaning there is a win
         return true;
       }
     }
   }
+
 }
-const container = document.querySelector('.container')
-const startButton = document.querySelector('#startGame');
-const key = document.createElement('div');
-const space = document.createElement('br');
-key.innerText = 'Player 1 is Yellow, Player 2 is Red'
-key.classList.add('key');
-startButton.addEventListener('click', e => {
-  e.preventDefault();
-  makeBoard(); //make js baord of arrays
-  makeHtmlBoard(); //make HTML board (table)
-  container.append(thePlayer);
-  thePlayer.append(playerNow);
-  container.append(space);
-  container.append(key);
-  startButton.remove();
-})
 
 
